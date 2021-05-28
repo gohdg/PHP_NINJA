054be0cf550d796d548cbb2e35e080265670ebc4
gohdg <gohdg@naver.com>
2021-05-28 15:51:50 +0900

    variable: $ and const define('NAME', value)

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-    echo "Hello Ninja";
+    // echo "Hello Ninja";
+    define('NAME', 'mario');
+    $name="Yoshi";
+    $age=20;
+
 ?>
 
 <!DOCTYPE html>
@@ -8,10 +12,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>my first PHP file</title>
 </head>
 <body>
-    <h1>asdfasf</h1>
-    <div class="aaa">asdfdf</div>
+    <h1>User Profile Page</h1>
+    <div><?php echo $name ?></div>
+    <div><?php echo $age ?></div>
+    <div><?php echo NAME ?></div>
 </body>
 </html>
